5a3ef561508caae88a482e97ecc652bc604ff79b
Dave Pcolar <dave@dpn.org>
2017-12-08 15:26:03 -0500

    Modify endpoints to retireve URL and token from environment

--- get_api_endpoint.py
+++ get_api_endpoint.py
@@ -1,31 +1,47 @@
 #!/usr/bin/python
-""" Execution shell for DPN API get calls """
+""" DPN API call shell """
 
 from app.dpn_python_library import *
 import requests
 import sys
+import os
 
-def get_dpn_api(endpoint_string):
+def get_dpn_api(url, token, endpoint_string):
     """ Call service for dpn API  """
-    dpn = {'url': 'http://ec2-54-226-144-188.compute-1.amazonaws.com'}
-    dpn_headers={'Content-Type': 'application/json','Accept': 'application/json', 'Authorization': 'Token token=dpn_token'}
-    # log_message("endpoint string: "+endpoint_string)
+    dpn = {'url': url}
+    dpn_headers={'Content-Type': 'application/json','Accept': 'application/json'}
+    dpn_headers['Authorization']="Token token="+token
     if len(endpoint_string) is 0:
         log_message("Endpoint and query string is required")
         return -1
     else:
-	# log_message("semding: " + dpn['url']+endpoint_string)
+	#log_message("sending: " + dpn['url']+endpoint_string)
+        #print dpn_headers
         response = requests.get(dpn['url']+endpoint_string, headers=dpn_headers)
         # log_message("Return code: " + str(response.status_code))
         print(response.text)
     return response.status_code
+
+# Retrieve environment variables
+if  "dpn_host" in os.environ:
+    dpn_host = os.environ['dpn_host']
+else:
+    log_message("Expecting: dpn_host")
+    exit(1)
+if  "dpn_token" in os.environ:
+    dpn_token = os.environ['dpn_token']
+else:
+    log_message("Expecting: dpn_token")
+    exit(1)
+#log_message("DPN Host: "+dpn_host)
+#log_message("DPN Headers: "+json.dumps(dpn_headers))
 
 # Read content from stdin
 dpn_querystring=sys.stdin.read().replace('\n', '')
 # log_message("length of querystring input " + str(len(dpn_querystring)))
 if len(dpn_querystring) is 0:
     exit(1)
-if get_dpn_api(dpn_querystring) is 200:
+if get_dpn_api(dpn_host, dpn_token, dpn_querystring) is 200:
     exit(0)
 exit(1)
 
